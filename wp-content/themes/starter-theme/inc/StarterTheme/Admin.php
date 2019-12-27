4dc56d8eb5625153dcfae6ec3b50f51898b46d2f
Mathieu THOLLET <mathieu.thollet@gmail.com>
2019-12-27 15:32:34 +0100

    small fix

--- wp-content/themes/starter-theme/inc/StarterTheme/Admin.php
+++ wp-content/themes/starter-theme/inc/StarterTheme/Admin.php
@@ -12,7 +12,7 @@ class Admin
     /**
      * Correction du bug faisant parfois apparaitre "wp/" dans redirect_to
      */
-    static function loginUrl($loginUrl)
+    public static function loginUrl($loginUrl)
     {
         $loginUrl = str_replace('%2Fwp%2Fwp-admin%2F', '%2Fwp-admin%2F', $loginUrl);
         return $loginUrl;
